40701a9a769c11f1dd509ef9df0dfab7e7832d10
Jason Tsorlinis <jtsorlinis@gmail.com>
2022-08-07 01:11:55 +1000

    added more sounds and obstacles on level 1

--- src/scenes/Level1.ts
+++ src/scenes/Level1.ts
@@ -1,12 +1,20 @@
 import Phaser from "phaser";
 
 export default class Level1 extends Phaser.Scene {
-  cursors: any;
+  startX = 50;
+  cursors!: {
+    left: any;
+    right: any;
+    up: any;
+    down?: Phaser.Input.Keyboard.Key;
+    space?: Phaser.Input.Keyboard.Key;
+    shift?: Phaser.Input.Keyboard.Key;
+  };
   player!: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
   text!: Phaser.GameObjects.Text;
   hasStar: boolean = false;
+  hasShoes: boolean = false;
   moveSpeed = 250;
-  startX = 4300;
   deathCount = 0;
   finished = false;
   deathSound!: Phaser.Sound.BaseSound;
@@ -19,17 +27,22 @@ export default class Level1 extends Phaser.Scene {
   preload() {
     this.load.image("player", "./assets/sylveon.png");
     this.load.image("star", "./assets/star.png");
-    this.load.image("apple", "./assets/apple.webp");
+    this.load.image("envelope", "./assets/envelope.png");
+    this.load.image("shoes", "./assets/shoes.png");
 
     this.load.audio("jump", "./assets/jump.mp3");
     this.load.audio("sylveon", "./assets/sylveon.mp3");
     this.load.audio("starMusic", "./assets/starmusic.mp3");
     this.load.audio("victorySound", "./assets/victory.mp3");
+    this.load.audio("skid", "./assets/skid.wav");
+    this.load.audio("powerup", "./assets/powerup.mp3");
+    this.load.audio("whoosh", "./assets/whoosh.mp3");
   }
 
   create() {
     this.hasStar = false;
     this.finished = false;
+    this.hasShoes = false;
     this.sound.stopAll();
 
     this.deathSound = this.sound.add("sylveon");
@@ -51,7 +64,7 @@ export default class Level1 extends Phaser.Scene {
       .sprite(this.startX, 400, "player")
       .setScale(0.5);
     this.player.setGravity(0, 1000);
-    this.player.setBounce(0.1);
+    this.player.setBounce(0.2);
     this.player.setSize(50, 115);
 
     // Create platforms
@@ -65,7 +78,7 @@ export default class Level1 extends Phaser.Scene {
     this.physics.add.collider(this.player, platforms);
 
     // Show text for star
-    const starText = this.add.text(2000, 300, "YOU GOT THIS... AH", {
+    const starText = this.add.text(2200, 300, "NICE... AH", {
       fontSize: "200px",
       fontStyle: "Bold",
       align: "center",
@@ -85,21 +98,22 @@ export default class Level1 extends Phaser.Scene {
 
     //Fake traps
     const fakeTraps = this.physics.add.group();
-    fakeTraps.add(this.physics.add.sprite(150, 400, "apple").setScale(0.2));
-    fakeTraps.add(this.physics.add.sprite(250, 300, "apple").setScale(0.2));
-    fakeTraps.add(this.physics.add.sprite(430, 400, "apple").setScale(0.2));
-    fakeTraps.add(this.physics.add.sprite(1050, 500, "apple").setScale(0.2));
-    fakeTraps.add(this.physics.add.sprite(1080, 500, "apple").setScale(0.2));
+    fakeTraps.add(this.physics.add.sprite(150, 400, "envelope").setScale(0.2));
+    fakeTraps.add(this.physics.add.sprite(250, 300, "envelope").setScale(0.2));
+    fakeTraps.add(this.physics.add.sprite(430, 400, "envelope").setScale(0.2));
+    fakeTraps.add(this.physics.add.sprite(1050, 500, "envelope").setScale(0.2));
+    fakeTraps.add(this.physics.add.sprite(1080, 500, "envelope").setScale(0.2));
 
     this.physics.add.collider(this.player, fakeTraps, () => {
       this.deathSound.play();
     });
 
     // Trap 1
-    const trap1 = this.physics.add.sprite(210, 400, "apple").setScale(0.2);
+    const trap1 = this.physics.add.sprite(210, 400, "envelope").setScale(0.2);
     const trap1Zone = this.add.zone(220, 500, 50, 500);
     this.physics.add.existing(trap1Zone);
     this.physics.add.overlap(this.player, trap1Zone, () => {
+      this.sound.play("whoosh");
       trap1.setVelocityY(300);
       trap1Zone.destroy();
     });
@@ -108,10 +122,11 @@ export default class Level1 extends Phaser.Scene {
     });
 
     // Trap 2
-    const trap2 = this.physics.add.sprite(400, 400, "apple").setScale(0.2);
+    const trap2 = this.physics.add.sprite(400, 400, "envelope").setScale(0.2);
     const trap2Zone = this.add.zone(700, 300, 400, 100);
     this.physics.add.existing(trap2Zone);
     this.physics.add.overlap(this.player, trap2Zone, () => {
+      this.sound.play("whoosh");
       trap2.setVelocityX(700);
       trap2Zone.destroy();
     });
@@ -120,10 +135,11 @@ export default class Level1 extends Phaser.Scene {
     });
 
     // Trap 3
-    const trap3 = this.physics.add.sprite(450, 580, "apple").setScale(0.2);
+    const trap3 = this.physics.add.sprite(450, 580, "envelope").setScale(0.2);
     const trap3Zone = this.add.zone(460, 500, 50, 400);
     this.physics.add.existing(trap3Zone);
     this.physics.add.overlap(this.player, trap3Zone, () => {
+      this.sound.play("whoosh");
       trap3.setVelocityY(-300);
       trap3Zone.destroy();
     });
@@ -132,15 +148,51 @@ export default class Level1 extends Phaser.Scene {
     });
 
     //Trap 4
-    const trap4 = this.physics.add.sprite(320, 200, "apple").setScale(0.2);
+    const trap4 = this.physics.add.sprite(320, 200, "envelope").setScale(0.2);
     const trap4Zone = this.add.zone(320, 500, 50, 400);
     this.physics.add.existing(trap4Zone);
     this.physics.add.overlap(this.player, trap4Zone, () => {
+      this.sound.play("whoosh");
       trap4.setVelocityY(800);
       trap4Zone.destroy();
     });
     this.physics.add.collider(this.player, trap4, () => {
       this.deathSound.play();
+    });
+
+    // Invisible wall
+    const invisWall = this.add.zone(1800, 400, 50, 500);
+    this.physics.add.existing(invisWall, true);
+    this.physics.add.collider(this.player, invisWall);
+
+    // Shoes
+    const shoes = this.physics.add.sprite(1750, 400, "shoes").setScale(0.15);
+    this.physics.add.overlap(this.player, shoes, () => {
+      this.sound.play("powerup");
+      shoes.destroy();
+      this.hasShoes = true;
+      star.setTint(0x00ff00);
+    });
+
+    // Shoes brake zone
+    const brakeZone = this.add.zone(4200, 400, 1000, 500);
+    this.physics.add.existing(brakeZone);
+    let braking = false;
+    this.physics.add.overlap(this.player, brakeZone, () => {
+      if (this.hasShoes) {
+        if (braking === false) {
+          braking = true;
+          this.player.setAccelerationX(-400);
+          this.sound.play("skid");
+        }
+        if (this.player.body.velocity.x < 10) {
+          this.player.setVelocityX(0);
+          this.player.setAccelerationX(0);
+          brakeZone.destroy();
+          this.sound.stopByKey("starMusic");
+          this.hasStar = false;
+        }
+      }
     });
 
     // Platform trap 1
@@ -171,17 +223,24 @@ export default class Level1 extends Phaser.Scene {
     const star = this.physics.add.sprite(1220, 500, "star").setScale(0.1);
     this.physics.add.overlap(this.player, star, () => {
       this.hasStar = true;
-      this.sound.play("starMusic", { rate: 1.1 });
-      this.player.setVelocityX(1000);
-      starText.setText("SLOW DOWN MIU MIU");
+      if (!this.hasShoes) {
+        starText.setText("SLOW DOWN MIU MIU");
+        this.sound.play("starMusic", { rate: 1.2 });
+        this.player.setVelocityX(1000);
+      } else {
+        this.sound.play("starMusic", { rate: 1.0 });
+        this.player.setVelocityX(750);
+      }
       star.destroy();
+      invisWall.destroy();
+      shoes.destroy();
     });
 
     // Setup input
     this.cursors = this.input.keyboard.createCursorKeys();
 
     // Setup camera
-    this.cameras.main.setBounds(0, 0, 13000, this.cameras.main.height);
+    this.cameras.main.setBounds(0, 0, 13500, this.cameras.main.height);
     this.cameras.main.setDeadzone(100);
     this.cameras.main.startFollow(this.player);
 
@@ -191,7 +250,7 @@ export default class Level1 extends Phaser.Scene {
   }
 
   update() {
-    if (!this.finished && this.player.x > 13500) {
+    if (!this.finished && this.player.x > 14000) {
       this.finished = true;
       this.victorySound.play();
       this.cameras.main.fadeOut(1000);
